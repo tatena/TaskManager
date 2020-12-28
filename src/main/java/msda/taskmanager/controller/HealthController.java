3e8272f62b9205818a336c44d593b4f45911c72f
tatena <ntate18@freeuni.edu.ge>
2020-12-28 20:03:19 +0400

    swagger added

---
+++ src/main/java/msda/taskmanager/controller/HealthController.java
@@ -0,0 +1,12 @@
+package msda.taskmanager.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+    @GetMapping("/health")
+    public void check() {
+        System.out.println("ok");
+    }
+}
