f35fcb3dd52e000633a6fc55dda1503d94ea6383
Levan Apakidze <lapak18@freeuni.edu.ge>
2021-01-03 18:59:13 +0400

    workspace dto object

--- src/main/java/msda/taskmanager/model/dto/WorkspaceDto.java
+++ src/main/java/msda/taskmanager/model/dto/WorkspaceDto.java
@@ -1,4 +1,11 @@
 package msda.taskmanager.model.dto;
 
+import lombok.Data;
+import msda.taskmanager.model.enums.WorkspaceStatus;
+
+@Data
 public class WorkspaceDto {
+    private String title;
+    private String description;
+    private WorkspaceStatus status;
 }
