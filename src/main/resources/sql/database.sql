fb139e40650926ee4c34ccca33223c779fa14856
Levan Apakidze <lapak18@freeuni.edu.ge>
2021-01-09 14:39:45 +0400

    final readme

--- src/main/resources/sql/database.sql
+++ src/main/resources/sql/database.sql
@@ -76,3 +76,14 @@ CREATE TABLE `task_manager`.`tasks`
 
 ALTER TABLE `task_manager`.`users`
     ADD COLUMN `deleted` BIT(1) NOT NULL AFTER `role`;
+
+ALTER TABLE `task_manager`.`tasks`
+    ADD COLUMN `start_date` DATETIME NOT NULL   AFTER `workspace_id`,
+    ADD COLUMN `end_date`   DATETIME NULL       AFTER `start_date`,
+    ADD COLUMN `deadline`   DATETIME NOT NULL   AFTER `end_date`;
+
+ALTER TABLE `task_manager`.`users`
+    ADD COLUMN `timezone` INT NULL AFTER `deleted`;
+
+ALTER TABLE `task_manager`.`workspaces`
+    ADD COLUMN `timezone` INT NULL AFTER `status`;
